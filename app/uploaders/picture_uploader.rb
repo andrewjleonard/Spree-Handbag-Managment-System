0e1cbb371636961305b642f529587ef820cc53a8
flinchtheone <professional@aleonard.co.uk>
2015-11-06 14:59:41 +0000

    Changed uploader gem

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,7 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [1000, 1000]
 
   if Rails.env.production?
-    storage :fog
+    storage :aws
   else
     storage :file
   end
@@ -23,7 +23,7 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # Provide a default URL as a default_urllt if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
