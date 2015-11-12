be43df034aceaf9c787d7acb7fba1ae888699a3b
flinchtheone <professional@aleonard.co.uk>
2015-11-12 15:54:41 +0000

    Updated storage location for images.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -20,7 +20,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.name.demodulize.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default_urllt if there hasn't been a file uploaded:
