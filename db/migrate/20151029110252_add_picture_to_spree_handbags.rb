5ac8bdf2bd39bbbb44a21a0862ef94aa07dd454a
flinchtheone <professional@aleonard.co.uk>
2015-11-12 16:38:46 +0000

    Testing multi upload.

--- db/migrate/20151029110252_add_picture_to_spree_handbags.rb
+++ db/migrate/20151029110252_add_picture_to_spree_handbags.rb
@@ -1,5 +1,6 @@
 class AddPictureToSpreeHandbags < ActiveRecord::Migration
   def change
     add_column :spree_handbags, :picture, :string
+    add_column :spree_handbags, :pictures, :json
   end
 end
