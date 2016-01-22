4af0bea9f5fa3fc6c5d4a235f7c28ad5258d94b9
flinchtheone <professional@aleonard.co.uk>
2016-01-22 08:25:45 +0000

    Fixed iPad file upload. Possibly

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -51,8 +51,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    @name = "#{SecureRandom.uuid}.#{file.extension}" if original_filename.present?
+  end
 
 end
