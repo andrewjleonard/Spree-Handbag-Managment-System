8197d1db0a929d2dc11304c3dfd90816597d467d
flinchtheone <professional@aleonard.co.uk>
2016-01-27 07:47:05 +0000

    Technical login ok

--- app/controllers/spree/admin/users_sessions_controller_decorator.rb
+++ app/controllers/spree/admin/users_sessions_controller_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Admin::UserSessionsController.class_eval do
   def redirect_back_or_default(default)
     if spree_current_user && spree_current_user.has_spree_role?("hms-technical")
-      redirect_to(admin_handbags_clean_path)
+      redirect_to('/admin/handbags/clean')
     elsif spree_current_user && spree_current_user.has_spree_role?("hms-admin")
     	redirect_to(admin_users_path)
     else
